6fe90aeb49dc7311a5594d7a201e5edeaef347b9
jswj30 <jswj30@naver.com>
2021-05-25 15:58:44 +0900

    add class

--- test_modal/src/App.js
+++ test_modal/src/App.js
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.scss";
 
-const App = () => {
-  return (
-    <div>
-      <h1>Modal</h1>
-      <button className="myBtn">Open Modal</button>
-      <div className="modal">
-        <div className="modal-content">
-          <span className="close">&times;</span>
-          <p>Some text in the modal..</p>
+class App extends Component {
+  handleViewModal = () => {
+    let modal = document.querySelector(".modal");
+    modal.style.display = "block";
+    console.log("test");
+  };
+
+  handleCloseModal = () => {
+    let modal = document.querySelector(".modal");
+    modal.style.display = "none";
+  };
+
+  handleWindowCloseModal = (event) => {
+    let modal = document.querySelector(".modal");
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Modal</h1>
+        <button className="myBtn" onClick={this.handleViewModal}>
+          Open Modal
+        </button>
+        <div className="modal" onClick={this.handleWindowCloseModal}>
+          <div className="modal-content">
+            <span className="close" onClick={this.handleCloseModal}>
+              &times;
+            </span>
+            <p>Some text in the modal..</p>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+// <button id="myBtn">Open Modal</button>
+// <div id="myModal" class="modal">
+//   <div class="modal-content">
+//     <span class="close">&times;</span>
+//     <p>Some text in the Modal..</p>
+//   </div>
+// </div>
 
 export default App;
