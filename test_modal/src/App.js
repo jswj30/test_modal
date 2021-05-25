7f13de6569280a0445f03165fb97fda4b0889404
jswj30 <jswj30@naver.com>
2021-05-25 10:38:35 +0900

    add scss

--- test_modal/src/App.js
+++ test_modal/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Modal</h1>
+      <button className="myBtn">Open Modal</button>
+      <div className="modal">
+        <div className="modal-content">
+          <span className="close">&times;</span>
+          <p>Some text in the modal..</p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
